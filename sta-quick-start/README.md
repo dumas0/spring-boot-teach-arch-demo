7bf711193c2449c4f4676a6f126cc6f6544d3770
dumas <dz.ma@shunwang.com>
2022-03-31 18:35:36 +0800

    junit test

--- sta-quick-start/README.md
+++ sta-quick-start/README.md
@@ -1,2 +1,3 @@
 # SpringBoot Initializr Quick Start
 
+
--- sta-quick-start/src/main/java/com/dumas/sta/quick/start/QuickStartApplication.java
+++ sta-quick-start/src/main/java/com/dumas/sta/quick/start/QuickStartApplication.java
@@ -1,7 +1,12 @@
 package com.dumas.sta.quick.start;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 /**
  * @author dumas
@@ -12,5 +17,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuickStartApplication.class,args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args ->{
+            System.out.println("--------------> springboot default bean <---------------");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(System.out::println);
+        };
     }
 }
