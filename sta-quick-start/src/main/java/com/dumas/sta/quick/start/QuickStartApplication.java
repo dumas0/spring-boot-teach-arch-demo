da24cd118af7a0e767051baf054e69bfdb6ce9b9
dumas <dz.ma@shunwang.com>
2022-03-31 14:50:18 +0800

    springboot initializr quick start

---
+++ sta-quick-start/src/main/java/com/dumas/sta/quick/start/QuickStartApplication.java
@@ -0,0 +1,16 @@
+package com.dumas.sta.quick.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/03/31 14:35
+ */
+@SpringBootApplication
+public class QuickStartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStartApplication.class,args);
+    }
+}
---
+++ sta-quick-start/src/test/java/QuickStartApplicationTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/03/31 14:43
+ */
+@SpringBootTest
+public class QuickStartApplicationTest {
+
+    @Test
+    public void contextLoads(){
+        System.out.println("Hello World!");
+    }
+}
