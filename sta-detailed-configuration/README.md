d41e78b5c531ca9d5e45e8d33511ee950a0adc51
dumas <dz.ma@shunwang.com>
2022-04-02 16:03:11 +0800

    properties configuration & multi-env configuration

---
+++ sta-detailed-configuration/README.md
@@ -0,0 +1,1 @@
+# SpringBoot Detailed Configuration & Multi-Env Configuration
---
+++ sta-detailed-configuration/src/main/java/com/dumas/sta/detailed/configuration/DetailedConfigurationApplication.java
@@ -0,0 +1,16 @@
+package com.dumas.sta.detailed.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/04/02 15:31
+ */
+@SpringBootApplication
+public class DetailedConfigurationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DetailedConfigurationApplication.class, args);
+    }
+}
