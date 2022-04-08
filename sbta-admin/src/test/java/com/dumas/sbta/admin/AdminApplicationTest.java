e352adb174c8a25c21c6b3c042962786e7564ef7
dumas <dz.ma@shunwang.com>
2022-04-08 08:08:06 +0800

    springboot admin

---
+++ sbta-admin/src/test/java/com/dumas/sbta/admin/AdminApplicationTest.java
@@ -0,0 +1,16 @@
+package com.dumas.sbta.admin;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/04/08 07:53
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AdminApplication.class)
+public class AdminApplicationTest {
+
+}
