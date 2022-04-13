556498bd826da439c8d3ce498ba233a68ad28ed5
dumas <dz.ma@shunwang.com>
2022-04-13 21:45:24 +0800

    unit test

---
+++ sbta-unit-test/src/main/java/com/dumas/sbta/unit/test/UnitTestApplication.java
@@ -0,0 +1,16 @@
+package com.dumas.sbta.unit.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/04/13 21:30
+ */
+@SpringBootApplication
+public class UnitTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestApplication.class, args);
+    }
+}
---
+++ sbta-unit-test/src/main/java/com/dumas/sbta/unit/test/entity/User.java
@@ -0,0 +1,15 @@
+package com.dumas.sbta.unit.test.entity;
+
+import lombok.Data;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/04/13 21:32
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+}
