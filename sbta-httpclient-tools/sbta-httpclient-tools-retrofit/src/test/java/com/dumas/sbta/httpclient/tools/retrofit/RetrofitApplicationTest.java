d49d49c055c6c38827bbb72d2a0b2178b5925d50
dumas <dz.ma@shunwang.com>
2022-05-05 17:34:54 +0800

    retrofit 调用

---
+++ sbta-httpclient-tools/sbta-httpclient-tools-retrofit/src/test/java/com/dumas/sbta/httpclient/tools/retrofit/RetrofitApplicationTest.java
@@ -0,0 +1,15 @@
+package com.dumas.sbta.httpclient.tools.retrofit;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author dumas
+ * @date 2022/05/05 5:24 PM
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RetrofitApplication.class)
+public class RetrofitApplicationTest {
+
+}
